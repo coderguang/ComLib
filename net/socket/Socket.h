d31cd51d4355d69435713eb0b1d9c27dbe802f14
royalchen in centos <royalchen@royalchen.com>
2015-08-18 21:43:24 +0800

    the GSocket header file

---
+++ net/socket/Socket.h
@@ -0,0 +1,56 @@
+#ifndef _COMMON_NET_SOCKET_SOCKET_H_
+#define _COMMON_NET_SOCKET_SOCKET_H_
+
+#include "SocketBase.h"
+#include "../../util/FuncPtr.h"
+
+namespace GCommon{
+  namespace GNet{
+    namespace GSocket{
+
+using namespace GCommon::GUtil;
+
+static const int MAX_BUF_SIZE=10240;//the socket read and write buf size
+//static const FD_SETSIZE=1024;
+static const int MAX_USER_SIZE=99999;
+
+class GSocket{
+  public:
+    GSocket(int ioType,int family,int sockType,int protocol,int port,int backlog);
+    
+    bool init(voFuncInt *newConnect,voFuncIntCharptr *newData,voFuncInt *disconnect,voFuncIntInt *except);
+
+    bool send(int sockfd,char *buf);
+   
+    bool close(int sockfd);
+  
+  protected:
+    void SelectLoop();
+    
+    void PollLoop();
+ 
+    void EpollLoop();
+
+  private:
+    GSocket(GSocket&)=delete;
+    GSocket operator=(GSocket&)=delete;
+  
+    char buf[MAX_BUF_SIZE];
+    
+    int counter;//the sockfd counter
+
+    fd_set rset,allset;//use for select mode
+   
+    int sclient[FD_SETSIZE];//use for select mode
+    
+    struct epollfd eclient[MAX_USER_SIZE];
+    
+
+
+};
+
+
+    }
+  }
+}
+#endif
---
+++ util/FuncPtr.h
@@ -0,0 +1,14 @@
+#ifndef _COMMON_UTIL_FUNCPTR_H_
+#define _COMMON_UTIL_FUNCPTR_H_
+
+namespace GCommon{
+  namespace GUtil{
+
+
+using voFuncInt=void (*)(int);
+using voFuncIntCharptr=void (*)(int,char*);
+using voFuncIntInt=void (*)(int,int);
+
+  }
+}
+#endif
