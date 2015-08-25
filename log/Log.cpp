70bec27cd052a7200db381dbeb813f4dcbbb703e
coderguang <coderguang@gmail.com>
2015-08-25 14:51:04 +0800

    整理头文件

--- log/Log.cpp
+++ log/Log.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "Log.h"
 using namespace GCommon::GLog;
 
---
+++ test/dllTest/dllTest/dllTest/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#pragma comment(lib,"..\\..\\..\\..\\winlib\\log.lib")
+int main(int argc, char **argv){
+	
+
+
+}
+
+
