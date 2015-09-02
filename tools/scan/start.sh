9b67e779f11acbaeb881c1b769335f563248a77a
royalchen in centos <royalchen@royalchen.com>
2015-09-02 21:33:31 +0800

    nc to scan ip

---
+++ tools/scan/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#guangdong
+
+./scan 183.0 gd &
+./scan 59.32 gd &
+./scan 125.88 gd &
+./scan 61.140 gd &
+./scan 121.32 gd &
+
+
+#guangXi
+./scan 171.104 gx &
+./scan 113.12 gx &
+./scan 61.139 gx &
+./scan 103.22 gx &
+./scan 116.1 gx &
+
