47f5c865bac58761424c2027bd97efc427f36a0b
royalchen in centos <royalchen@royalchen.com>
2015-09-21 21:28:45 +0800

    read db data complete

---
+++ Db/hacker/dorking/checkT.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import ConfigParser
+import MySQLdb
+import re
+import sys
+
+#read config.ini
+config=ConfigParser.ConfigParser()
+config.readfp(open('../../../../config.ini'))
+
+host=config.get("dbconfig","host")
+user=config.get("dbconfig","user")
+passwd=config.get("dbconfig","passwd")
+
+#connect to mysql db
+
+conn=MySQLdb.connect(host=host,user=user,passwd=passwd)
+conn.select_db('hacker')
+
+#get curs point
+curs=conn.cursor()
+
+pattern=re.compile(r'\d+')
+
+for i in range(254):
+  tableName="dorking_"+str(i+1)
+  checkStr="select ip from "+tableName+" where p"+str(sys.argv[1])+"='1\'"
+  curs.execute(checkStr)
+  #get the result rowcount
+  numrows=int(curs.rowcount)
+  results=curs.fetchall()
+  for line in results:
+    ipStr=line[0]
+    print ipStr
+      
+#free the curs
+curs.close();
+
+#close the connection
+conn.close();
+
