3244bce085d32f1155e9e966c3fd4c806ed72e84
server <royalchen@royalchen.com>
2015-10-08 21:30:09 +0800

    modified:   test/udp/client 	modified:   test/udp/client.cpp

--- test/udp/client.cpp
+++ test/udp/client.cpp
@@ -29,11 +29,11 @@ int main(int argc,char **argv){
   char msg[1024];
   char sendBuf[1024]="hello,udp";
 
-  CSocketBase::Connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+  //CSocketBase::Connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
 
   for(int i=0;i<10;i++){
-    //sendto(sockfd,sendBuf,1024,0,(struct sockaddr*)&servaddr,len);
-    CSocketBase::Writen(sockfd,sendBuf,sizeof(sendBuf));
+    sendto(sockfd,sendBuf,sizeof(sendBuf),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+    //CSocketBase::Writen(sockfd,sendBuf,sizeof(sendBuf));
     
   }
 
