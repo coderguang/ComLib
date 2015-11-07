617a01e44e654a52ae9390db6605af74b8598591
server <royalchen@royalchen.com>
2015-11-07 11:01:33 +0800

    before  nmap

--- tools/scan/one.sh
+++ tools/scan/one.sh
@@ -6,11 +6,11 @@ ip=$1
 
 echo "start scanning..." 
 
-function scan(){
-for i in `seq 1 10000`;do
-      nc -z $ip $i
-done 
-}
+#function scan(){
+#for i in `seq 1 65535`;do
+      nc -z $ip 1-65535
+#done 
+#}
 
-scan 
+#scan 
 echo "end scanning..."
